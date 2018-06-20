1f87e08fdeb153a389a04e9a05501a0298826734
Giang Nguyen <gianggiangednguyen@outlook.com>
2018-06-20 15:41:18 +0700

    update
    update

---
+++ Components/HotProductsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Lazarus.Data;
+using Lazarus.Models;
+
+namespace Lazarus.Components
+{
+    public class HotProductsViewComponent : ViewComponent
+    {
+        private readonly LazarusDbContext _context;
+
+        public HotProductsViewComponent(LazarusDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+
+            return View();
+        }
+    }
+}
